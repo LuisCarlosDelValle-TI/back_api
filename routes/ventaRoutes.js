ffeee7ed3852f1d995766dd51015affcfd70b6d4
Luis Carlos Del Valle <luisdelvalledelvlle@gmail.com>
2026-03-20 23:24:17 -0600

    actualice ruta de ventas

--- routes/ventaRoutes.js
+++ routes/ventaRoutes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { obtenerVentas, crearVenta } = require('../controllers/ventaController');
 
-router.get('/', obtenerVentas);
+router.get('/:id', obtenerVentas);
 router.post('/', crearVenta);
 
 
