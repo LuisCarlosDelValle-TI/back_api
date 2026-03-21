50ac013f1334db5b04c5b6e9251f16f92380b415
Luis Carlos Del Valle <luisdelvalledelvlle@gmail.com>
2026-03-20 22:15:35 -0600

    actualice cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ require('dotenv').config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(cors({ origin: 'https://dashboard-angular-five.vercel.app' }));
 
 // Configuraciones de Base de Datos
 const conectarMongo = require('./config/mongo');
